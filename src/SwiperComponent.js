0812eb266671b6967a5f1bafa8ef825f328b6028
KashanMehmood <kashan.mehmood13@gmail.com>
2020-04-27 00:00:12 +0500

    slide 1

--- src/SwiperComponent.js
+++ src/SwiperComponent.js
@@ -1,22 +1,90 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import Swiper from 'react-native-swiper';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 class SwiperComponent extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text>SwiperComponent Component</Text>
-            </View>
+            <Swiper
+                loop={true}
+                style={styles.container}
+            >
+
+                {/* Slide 1 */}
+
+                <View style={styles.slide}>
+                    <View style={styles.header}>
+                        <Image
+                            style={styles.image}
+                            source={require('./asset/asset1.png')}
+                            resizeMode="stretch"
+                        />
+                    </View>
+                    <View style={styles.footer}>
+                        <Text style={styles.title}>Cloud Storage</Text>
+                        <Text style={styles.text}>
+                            Thank you for watching my video and hope to get more tips for the next videos better.
+                        </Text>
+                    </View>
+                </View>
+
+                {/* Slide 1 End */}
+
+            </Swiper>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
+
+    container: {},
+    slide: {
+
         flex: 1,
+        backgroundColor: 'blue',
+    },
+    slide2: {
+
+        flex: 1,
+        backgroundColor: 'blue',
+    },
+    header: {
+
+        flex: 2,
+        backgroundColor: 'white',
         justifyContent: 'center',
-        alignItems: 'center'
-    }
+        alignItems: 'center',
+    },
+    footer: {
+
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    image: {
+
+        width: '80%',
+        height: '80%',
+        // flex: 0.7,
+        // backgroundColor: 'green',
+    },
+    title: {
+
+        fontSize: 25,
+        fontWeight: "bold",
+        textAlign: "center",
+        color: '#3465d9',
+    },
+    text: {
+
+        textAlign: "center",
+        color: 'gray',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 18,
+        marginLeft: 30,
+        marginRight: 30,
+        // backgroundColor: 'green',
+    },
 });
 
 export default SwiperComponent;
